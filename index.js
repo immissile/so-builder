6d78dea8583de0fd864021c023ece0ff7f29580f
immissile <shaohualou@gmail.com>
2015-06-14 04:06:28 +0800

    important version: add browserify; add sourcemaps; fix bugs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 var _ = require('lodash-node'),
+    clc = require('cli-color'),
     less = require('gulp-less'),
+    browserify = require('gulp-browserify'),
+    html = require('html-browserify'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
@@ -8,11 +11,12 @@ var _ = require('lodash-node'),
     notify = require('gulp-notify'),
     rimraf = require('gulp-rimraf'),
     rev = require('gulp-rev'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    source = require('vinyl-source-stream'),
+    sourcemaps = require('gulp-sourcemaps');
 
-    //cache = require('gulp-cache'),
-    //imagemin = require('gulp-imagemin'),
-    //sourcemaps = require('gulp-sourcemaps');
+    //cache = require('gulp-cache')
+    //imagemin = require('gulp-imagemin')
 
 module.exports = function(gulp, options){
     var defaults = {
@@ -25,32 +29,81 @@ module.exports = function(gulp, options){
         cssRevName: 'rev-css.json',
 
         jsRoot: 'scripts/js',
+        jsSource: 'scripts/source',
         jsDist: 'dist/js',
         globalJson: 'global.json',
         jsRevName: 'rev-js.json',
+        
+        templateRoot: 'template',
 
         autoReload: false,
         notify: false,
-        showGlobalJS: true, // global.json列表
+        showGlobalJS: true, // show global.json file list ?
         sourcemaps: false,
         debug: false
     };
     var op = _.extend(defaults, options);
+    
+    // for display debug msg
+    function debug(msg) {
+        return console.log(clc.yellow(clc.bold('debug msg: ') + msg));
+    }
+    
+    // buildLess
+    function buildLess(file){
+        file = file || [op.appRoot + op.lessRoot + '/**/*.less'];
+        return gulp.src(file)
+            .pipe(sourcemaps.init())
+            .pipe(less())
+            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+            .pipe(sourcemaps.write())
+            //.pipe(gulp.dest(op.appRoot + op.cssRoot))
+            //.pipe(minifycss())
+            .pipe(gulp.dest(op.appRoot + op.cssRoot));
+    }
+    
+    // browserify
+    function _browserify(file){
+        file = file || [op.appRoot + op.jsSource + '/**/*.js'];
+        return gulp.src(file)
+            .pipe(sourcemaps.init())
+            .pipe(browserify({
+                //nobuiltins: 'events querystring',
+                //debug: true,
+                transform: html,
+                insertGlobals: false,
+                ignore: 'jquery',
+                /*resolve: function(mod){
+                    console.log('load module:', mod);
+                }*/
+            }))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(op.appRoot + op.jsRoot));
+    }
+    
     if (op.debug) {
-        console.log(op);
+        debug(op);
     }
     op.appRoot = op.appRoot + '/';
     
-    var globalList = require(op.appRoot + op.jsRoot +'/global.json');
-
+    var globalList = [];
+    try {
+        globalList = require(op.appRoot + op.jsSource +'/global.json');
+    } catch (e) {
+        console.log(clc.red(clc.bold('Error: ') + 'missing global.json'));
+        console.log(clc.yellow(clc.bold('Tips: ') + 'create global.json in folder ' + op.jsSource + '/'));
+    }
+    
+    if (op.debug) {
+        debug('globalList file:');
+        _.each(globalList, function(file){
+            console.log(clc.cyan(clc.bold('global file: '), file));
+        });
+    }
+    
     // for styles
     gulp.task('styles', function() {
-        var stream = gulp.src(op.appRoot + op.lessRoot + '/**/*.less')
-            .pipe(less())
-            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-            .pipe(gulp.dest(op.appRoot + op.cssRoot))
-            .pipe(minifycss())
-            .pipe(gulp.dest(op.appRoot + op.cssRoot));
+        var stream = buildLess();
         if (op.notify) {
             return stream.pipe(notify({
                 message: 'less file build complete'
@@ -59,18 +112,37 @@ module.exports = function(gulp, options){
             return stream;
         }
     });
+    
+    // build global.js
+    gulp.task('globalScript', function(){
+        if (op.showGlobalJS) {
+            if (globalList.length > 0) {
+                console.log(clc.magenta('concat global.json:'));
+                _.each(globalList, function(file){
+                    console.log(clc.cyan(clc.bold('global file: '), file));
+                });
+            } else {
+                console.log(clc.yellow('global scripts list is empty'));
+            }
+        }
+        var stream = gulp.src(globalList)
+            //.pipe(sourcemaps.init())
+            .pipe(concat('global.js'))
+            //.pipe(sourcemaps.write())
+            .pipe(gulp.dest(op.appRoot + op.jsRoot));
+        if (op.notify) {
+            return stream.pipe(notify({
+                message: 'global.js build complete'
+            }));
+        } else {
+            return stream;
+        }
+    });
 
     // for scripts
     gulp.task('scripts', function() {
-        if (op.showGlobalJS) {
-            console.log('concat global.json:');
-            _.each(globalList, function(file){
-                console.log('global file: ', file);
-            });
-        }
-        var stream = gulp.src(globalList)
-            .pipe(concat('global.js'))
-            .pipe(gulp.dest(op.appRoot + op.jsRoot));
+        var stream = _browserify();
+        
         if (op.notify) {
             return stream.pipe(notify({
                 message: 'global.js build complete'
@@ -99,19 +171,8 @@ module.exports = function(gulp, options){
     });
 
     // build js
-    gulp.task('build-js', function(){
-        console.log('concat global.json:');
-        _.each(globalList, function(file){
-            console.log('global file: ', file);
-        });
-        gulp.src(globalList)
-            .pipe(concat('global.js'))
-            .pipe(gulp.dest(op.appRoot + op.jsRoot));
-        console.log('global.js build completed');
-        
+    gulp.task('build-js', ['globalScript'], function(){
         return gulp.src(op.appRoot + op.jsRoot + '/**/*.js')
-            //.pipe(concat(''))
-            //.pipe(gulp.dest('dist/js'))
             .pipe(uglify())
             .pipe(rev())
             .pipe(gulp.dest(op.appRoot + op.jsDist))
@@ -131,18 +192,26 @@ module.exports = function(gulp, options){
     });
 
     // watch
-    gulp.task('watch', function() {
+    gulp.task('watch', ['styles', 'globalScript', 'scripts'], function() {
         if (op.autoReload) {
             livereload.listen();
         }
 
-        gulp.start('styles', 'scripts');
-
-        gulp.watch(op.appRoot + op.lessRoot + '/**/*.less', ['styles']);
+        // watch less
+        gulp.watch(op.appRoot + op.lessRoot + '/**/*.less', function(file){
+            console.log(clc.green(clc.bold('less file ' + file.type + ': '), file.path));
+            buildLess(file.path);
+        });
+        
+        // watch js
         gulp.watch([
-            op.appRoot + op.jsRoot + '/**/*.js',
-            op.appRoot + op.jsRoot + '/global.json'
-        ], ['scripts']);
+            op.appRoot + op.jsSource + '/**/*.js',
+            op.appRoot + op.jsSource + '/global.json'
+        ], function(file){
+            console.log(clc.green(clc.bold('js file ' + file.type + ': '), file.path));
+            //if ()
+            _browserify(file.path);
+        });
 
         //gulp.watch('src/images/**/*', ['images']);
 
